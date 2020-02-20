e6c837e4c3571baad75558322a3cb1852046ea97
Taewoo Lee <lteawoo@naver.com>
2020-02-20 21:32:12 +0900

    PasswordEncode 추가, H2-console 세팅

--- src/main/java/kr/taeu/restsecurity/config/AppConfig.java
+++ src/main/java/kr/taeu/restsecurity/config/AppConfig.java
@@ -1,5 +1,25 @@
 package kr.taeu.restsecurity.config;
 
-public class AppConfig {
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+public class AppConfig {
+	/*
+	 * 스프링 시큐리티 암호화 알고리즘 Bean 등록
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		//return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Bean
+	public ServletRegistrationBean<WebServlet> h2servletRegistration() {
+		ServletRegistrationBean<WebServlet> registrationBean = new ServletRegistrationBean<WebServlet>(new WebServlet());
+		registrationBean.addUrlMappings("/h2-console/*");
+		return registrationBean;
+	}
 }
