9658da6ed22d27c17a99d246994cc862b73e1951
Taewoo Lee <lteawoo@naver.com>
2020-02-26 09:55:02 +0900

    정리

--- src/main/java/kr/taeu/restsecurity/member/exception/EmailNotFoundException.java
+++ src/main/java/kr/taeu/restsecurity/member/exception/EmailNotFoundException.java
@@ -2,7 +2,6 @@ package kr.taeu.restsecurity.member.exception;
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import kr.taeu.restsecurity.global.error.exception.ObjectNotFoundException;
 import kr.taeu.restsecurity.member.domain.model.Email;
 
 public class EmailNotFoundException extends UsernameNotFoundException {
